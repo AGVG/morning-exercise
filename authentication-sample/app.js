f2b3e19d907fb02d8ac0bc2a93fb3aa6e5855f10
Anton Virtanen <avirtanen@hotmail.fr>
2017-03-03 14:59:52 +0100

    should work now

---
+++ authentication-sample/app.js
@@ -0,0 +1,71 @@
+const express      = require('express');
+const path         = require('path');
+const favicon      = require('serve-favicon');
+const logger       = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const session      = require("express-session");
+const passport     = require("passport");
+const cors         = require('cors');
+
+
+const authController = require("./routes/auth-controller");
+const index          = require('./routes/index');
+const users          = require('./routes/users');
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/passport-local");
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+require('./config/passport')(passport);
+app.use(cors());
+app.use(session({
+  secret: "passport-local-strategy",
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', authController);
+app.use('/users', users);
+
+app.all('/*', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
